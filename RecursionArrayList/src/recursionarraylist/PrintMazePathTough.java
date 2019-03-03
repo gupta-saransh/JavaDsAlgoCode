35a93ccc829027eb4d4f9813c9deccac355b095b
Saransh Gupta <35869697+gupta-saransh@users.noreply.github.com>
2019-03-03 12:38:17 +0530

    Update PrintMazePathTough.java

--- RecursionArrayList/src/recursionarraylist/PrintMazePathTough.java
+++ RecursionArrayList/src/recursionarraylist/PrintMazePathTough.java
@@ -10,25 +10,29 @@ package recursionarraylist;
  * @author test
  */
 public class PrintMazePathTough {
+    static int n=1;
     static void pmpt(int cr,int cc,int dr,int dc,String sol)
     {
         if(cc==dc && cr==dr)
-        {System.out.println(sol);
+        {System.out.println(n+") "+sol);
+        n++;
         return;
         }
         if(cc>dc || cr>dr)
             return;
         
-        for(int i=1;i<dc;i++)
+        for(int i=1;i<dc+1;i++)
          pmpt(cr,cc+i,dr,dc,sol+"H"+i);
         
         
-       for(int i=1;i<dr;i++)
+       for(int i=1;i<dr+1;i++)
          pmpt(cr+i,cc,dr,dc,sol+"V"+i);
         
         
-      for(int i=1;i<dr&&i<dc;i++)
+      for(int i=1;i<dr+1&&i<dc+1;i++)
         pmpt(cr+i,cc+i,dr,dc,sol+"D"+i);
+               
+
     }
     public static void main(String args[])
     {
@@ -36,6 +40,7 @@ public class PrintMazePathTough {
     pmpt(0,0,2,2,"");
     
     }
+    
     
     
 }
